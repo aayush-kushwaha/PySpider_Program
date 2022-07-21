0af7f8be505739c440259507fb86db595b0645b5
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-21 23:12:23 +0530

    Continue_Example

---
+++ Loops/Break_Continue_Pass/CONTINUE_example.py
@@ -0,0 +1,6 @@
+# Continue is a keyword which is used to skip the current execution and make the control to go for next next iteration
+# Example
+for i in range(11):
+    if i == 4: # If i ==4 then control will skip the current execution when we write continue after that
+        continue # Here the control(PVM) will skip the output of above condition
+    print(i)
